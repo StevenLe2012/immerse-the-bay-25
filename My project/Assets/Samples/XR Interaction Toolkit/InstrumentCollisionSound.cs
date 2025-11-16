17b01bdbe4ff32cb0c09c056984a877101ee565a
jellyshroom <thejellyshroom@gmail.com>
2025-11-16 01:21:19 -0800

    working instruments

--- My project/Assets/Samples/XR Interaction Toolkit/InstrumentCollisionSound.cs
+++ My project/Assets/Samples/XR Interaction Toolkit/InstrumentCollisionSound.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
 
 [RequireComponent(typeof(AudioSource))]
 public class InstrumentCollisionSound : MonoBehaviour
@@ -7,30 +10,48 @@ public class InstrumentCollisionSound : MonoBehaviour
     [SerializeField] private float minPlayInterval = 0.4f;
 
     private AudioSource audioSource;
+    private XRGrabInteractable interactable;
     private float lastPlayTime = Mathf.NegativeInfinity;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        interactable = GetComponent<XRGrabInteractable>();
+    }
+
+    private void OnEnable()
+    {
+        if (interactable == null) return;
+        interactable.selectEntered.AddListener(HandleSelectEntered);
+    }
+
+    private void OnDisable()
+    {
+        if (interactable == null) return;
+        interactable.selectEntered.RemoveListener(HandleSelectEntered);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (Time.time - lastPlayTime < minPlayInterval) return;
         PlaySound();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (Time.time - lastPlayTime < minPlayInterval) return;
         PlaySound();
     }
 
-    private void PlaySound()
+    private void HandleSelectEntered(SelectEnterEventArgs args)
+    {
+        PlaySound();
+    }
+
+    public void PlaySound()
     {
         Debug.Log("Playing sound for " + gameObject.name);
 
         if (audioSource == null || audioSource.clip == null) return;
+        if (Time.time - lastPlayTime < minPlayInterval) return;
         audioSource.Stop();
         audioSource.Play();
         lastPlayTime = Time.time;
