6ed5e8ff0f1fb2a8002078e14947fc130bd4304c
SkyanSam <samuelwiggins15@gmail.com>
2025-11-15 20:50:59 -0800

    Kareoke display lyrics from VTT file

---
+++ My project/Assets/KaraokeText/KaraokeText.cs
@@ -0,0 +1,166 @@
+using JetBrains.Annotations;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using TMPro;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class Timestamp
+{
+    public int hours;
+    public int minutes;
+    public double seconds;
+
+    // NOTE! Make sure to use GetTotalSeconds if you are comparing with AudioSource.time
+
+    public double GetTotalSeconds()
+    {
+        return seconds + (60f * minutes) + (60f * 60f * hours);
+    }
+    public Timestamp(string text)
+    {
+        Debug.Log(text);
+        var split = text.Split(':');
+        hours = int.Parse(split[0]);
+        minutes = int.Parse(split[1]);
+        seconds = double.Parse(split[2]);
+    }
+}
+public class LyricalLine
+{
+    public Timestamp start;
+    public Timestamp end;
+    public string lyric;
+}
+public class KaraokeText : MonoBehaviour
+{
+    // The canvas UI gameobject.
+    public Canvas canvas;
+
+    // An instance that you can access this class from anywhere.
+    public static KaraokeText Instance;
+
+    // This string should be populated with the VTT data.
+    // Make sure this is the data and not the filename.
+    [TextArea]
+    public string str;
+
+    // The Text Lines that our lyrics are displayed.
+    public TextMeshProUGUI nextLine;
+    public TextMeshProUGUI currentLine;
+
+    // The rect transform for the above text lines.
+    RectTransform currentRectTransform;
+    RectTransform nextRectTransform;
+
+    // The lyrics.
+    public List<LyricalLine> lyrics = new List<LyricalLine>();
+
+    float timeElapsed = 0f;
+    bool isInitialized = false;
+    int currentIndex = 0;
+    
+    void Start()
+    {
+        // NOTE! You may not want to put this in the Start function.
+        // Call this function once you recieve the information for the
+        // audio and the subtitles.
+        InitKaraoke();
+        Instance = this;
+    }
+
+    // Read VTT Text that is in the str variable.
+    // If you need to use a different subtitle format (SRT) let me know on discord.
+    public void InitKaraoke()
+    {
+        lyrics = new List<LyricalLine>();
+        var lines = str.Split('\n');
+        bool isLyrics = false;
+        foreach (var line in lines)
+        {
+            if (isLyrics)
+            {
+                lyrics[lyrics.Count - 1].lyric = line;
+                isLyrics = false;
+            }
+            else
+            {
+                var timestamps = line.Split("-->");
+                if (timestamps.Length > 1)
+                {
+                    lyrics.Add(new LyricalLine());
+                    lyrics[lyrics.Count - 1].start = new Timestamp(timestamps[0]);
+                    lyrics[lyrics.Count - 1].end = new Timestamp(timestamps[1]);
+                    isLyrics = true;
+                }
+            }
+        }
+
+        // Set the rect transforms.
+        currentRectTransform = currentLine.GetComponent<RectTransform>();
+        nextRectTransform = currentLine.GetComponent<RectTransform>();
+
+        // Set the current index.
+        currentIndex = 0;
+
+        // NOTE!! Use this isInitialized variable if needed.
+        // Update Karaoke should only be called if isInitialized.
+        isInitialized = true;
+        
+        // Reinitialize time.
+        timeElapsed = 0;
+    }
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        // May want to add a checker for this if it works.
+        if (isInitialized)
+            UpdateKaraoke();
+    }
+    public void UpdateKaraoke()
+    {
+        // Move to the next lyric if we have finished this lyric.
+        if (lyrics[currentIndex].end.GetTotalSeconds() < GetAudioTime())
+        {
+            currentIndex++;
+        }
+
+        // Check if we have finished all the lyrics.
+        if (currentIndex >= lyrics.Count) return;
+
+        var lyric = lyrics[currentIndex];
+        var t = (GetAudioTime() - lyric.start.GetTotalSeconds()) / (lyric.end.GetTotalSeconds() - lyric.start.GetTotalSeconds());
+        int middle = (int)Mathf.Round((float)t * lyric.lyric.Length);
+        if (middle > 0)
+        {
+            currentLine.richText = true;
+            currentLine.text = $"<color=#FF00FF>{lyric.lyric.Substring(0, middle)}<color=#FFFFFF>{lyric.lyric.Substring(middle)}";
+        }
+        else
+        {
+            currentLine.text = lyric.lyric;
+        }
+
+        // Check if we can set the lyric of the nextLine if it exists.
+        // Else set it to nothing.
+        if (currentIndex + 1 < lyrics.Count)
+        {
+            nextLine.text = lyrics[currentIndex + 1].lyric;
+        }
+        else
+        {
+            nextLine.text = "";
+        }
+    }
+
+    // TODO! Replace this line with the AudioSource.time variable.
+    // If you want you can reset timeElapsed, but AudioSource.time is more accurate.
+    float GetAudioTime()
+    {
+        return timeElapsed;
+    }
+}
